bbfc225216a0b0f162a236e771fe8d9a5ea006bd
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-04-24 20:30:38 -0300

    add structural types examples

--- class-01-data-types/main.py
+++ class-01-data-types/main.py
@@ -1,3 +1,5 @@
+#Primitive Types
+
 milk = "Leite instegral"
 milk = "Leite desnatado"
 volume = 2
@@ -16,3 +18,54 @@ print("E tem " + life_cat + " vidas")
 
 print("O gato bebe " + milk + " e tem " + life_cat + " vidas" )
 print(f"O gato bebe {milk} e tem {life_cat} vidas")
+
+name = "Eduardo"
+age = 31
+
+is_admin = True #boolean: True or False
+
+print(is_admin)
+print(type(is_admin))
+
+# Structural Types(Collections)
+
+animes = ["Dragon ball", "Naruto", "Jojo", "Death Note"]
+              # =>  0          1         2       3
+              # <= -4         -3        -2      -1
+print(animes)
+print(type(animes))
+print(animes[0])
+print(animes[3])
+print(animes[-1])
+
+list = ["A", 123, True, ["B", "C"]]
+
+print(list[0])
+print(list[3])
+print(list[-1])
+print(list[3][0])
+
+list[2] = False
+list[-1][0] = "D" 
+print(list)
+print(len(list)) #length
+
+# Tuple
+tuple = ("A", 123, True)
+print(type(tuple))
+print(tuple[0])
+
+# tuple[1] = 789 #tuple is immutable
+print(tuple)
+
+# Dict
+dict = {
+  "name":"Arthur", 
+  "age": 31,
+  "is_admin": is_admin,
+}
+print(type(dict))
+print(dict)
+print(dict["name"])
+print(dict["is_admin"])
+print(dict["age"])
