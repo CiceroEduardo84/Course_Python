87e6a16bbac3eb1065cb27da9518cb89ab150485
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-04-25 19:40:52 -0300

    add challenge name

---
+++ class-01-data-types/challenges/main.py
@@ -0,0 +1,5 @@
+#Receba o nome do usuario via "input"
+#Atribua o nome a uma variavel e use a variavel
+#Imprima na tela uma mensagem de boa vindas com o nome
+name = input("Digite o nome:")
+print(f"Bem vindo, {name}!")
