c08427b27526cbbf02f17650df00a56786d376ea
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-04-25 20:36:08 -0300

    add operators orithmetics, assigments and Comparison

---
+++ class-02-operators/main.py
@@ -0,0 +1,41 @@
+import math
+
+num1 = 2
+num2 = 3
+
+# arithmetic
+
+print(num1 + num2)
+print(num1 - num2) 
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** num2)
+print(num1 % num2)
+
+print(pow(num1, num2))
+print(round(1.5))
+print(math.ceil(1.4))
+print(math.floor(1.9))
+
+# assignment
+num = 2
+# num = num + 1
+num += 1
+num -= 1
+num *= 1
+num /= 1
+num %= 1
+num **= 1
+
+print(num)
+
+# Comparison
+
+print(2 == 2)
+print(2 == 3)
+print(2 != 3)
+print(2 != 2)
+print(2 > 3)
+print(2 < 3)
+print(2 <= 3)
+print(3 >= 3)
