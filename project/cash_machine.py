063e3ec4b88510cb0958525b775cf3917e5b9038
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-05-09 21:09:03 -0300

    add methods check_statement, _load_transactions and withdraw

--- project/cash_machine.py
+++ project/cash_machine.py
@@ -2,17 +2,64 @@ class Bank_account:
   def __init__(self, balance=0):
     self.__balance = balance
     self.__file = "project/files/transactions.txt"
-  
+    self.__transactions = []
+    self.__load_transactions()
+
+  def check_statement(self):
+    print("==== Extrato ====")
+    for transaction in self.__transactions:
+      print(f"{transaction[0]}, {transaction[1]}")
+    print("=================")
+    print(f"Saldo (=): {self.__balance}")
+    print("=================")
+
+  def __load_transactions(self):
+    try:
+      with open(self.__file, "r") as file:
+        for line in file:
+          transaction, amount = line.strip().split(", ")
+          amount = float(amount)
+          
+          self.__transactions.append((transaction, amount))
+
+          if transaction == "deposito (+)":
+            self.__balance += amount
+          elif transaction == "saque (-)":
+            self.__balance -= amount
+    except:
+      print("Algo deu errado em abrir o arquivo!")
+      pass                    
+
   def deposit(self, amount):
     self.__balance += amount
 
     try:
       with open(self.__file, "a") as file:
         file.write(f"deposito (+), {amount}\n")
+        self.__transactions.append(("deposito (+)", amount))
     except:
       print("Algo deu errado ao abrir o arquivo!")
       pass
     print(f"Depósito de R${amount} realizado!")
+
+  def withdraw(self, amount):
+    if amount == 0:
+      return print("Saque deve ser meior que 0!")
+    
+    if amount <= self.__balance:
+      self.__balance -= amount
+
+      try:
+        with open(self.__file, "a") as file:
+          file.write(f"saque (-), {amount}\n")
+          self.__transactions.append(("saque (-)", amount))
+      except:
+        print("Algo deu errado ao abrir o arquivo!")
+        pass
+      print(f"Saque de R${amount} realizado!")
+    else:
+      print("Saldo insuficiente...")
+
 
 account = Bank_account()
 waiting_menu = False
@@ -37,12 +84,13 @@ while True:
   print("")
 
   if option == "1":
-    print("Extrato")
+    account.check_statement()
   elif option == "2":
     amount = float(input("Digite o valor para depositar: "))
     account.deposit(amount)
   elif option == "3":
-    print("Saque")
+    amount = float(input("Digite o valor para saque: "))
+    account.withdraw(amount)
   elif option == "4":
     print("Programa encerrado!\n")
     break
